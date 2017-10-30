c48fb8691f5a5460396235e8ea18d983bc3193c4
Yoon SeockChan <chaneyoon@gmail.com>
2017-10-31 01:26:52 +0900

    add get param

--- app.js
+++ app.js
@@ -5,18 +5,24 @@ app.use(express.static('image'));
 
 app.get('/', function(req, res) {
     var d = new Date();
-    var n = d.getTime();
 
     var print = "Hello World<br>";
     print += "You can see the image <a href='/image'>here</a>";
     print += "<p>I will tell you the time. </p>";
     print += "<h1>"+d+"</h1>";
+
+    if (typeof req.query.id === 'undefined')
+    {
+        req.query.id = "No input";
+    }
+
+    print += `<p>$_GET['id'] : ${req.query.id}</p>`;
     res.send(print);
     console.log("Connected");
 });
 
 app.get('/image', function(req, res) {
-    res.send("<h1>넘모넘모 재밋는거시다!</h1><img src='/dici.jpg'>");
+    res.send("<h1>nodejs는 넘모넘모 재밋는거시다!</h1><img src='/dici.jpg'>");
 });
 
 app.listen(3000, function() {
