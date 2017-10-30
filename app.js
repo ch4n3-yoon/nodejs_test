aaad678a6af3084a4a3cec8ac9dff64a8ff6a6b5
Yoon SeockChan <chaneyoon@gmail.com>
2017-10-31 01:05:44 +0900

    linked / to /image

--- app.js
+++ app.js
@@ -4,8 +4,14 @@ var app = express();
 app.use(express.static('image'));
 
 app.get('/', function(req, res) {
-    res.send("Hello World");
+    var print = "Hello World<br>";
+    print += "You can see the image <a href='/image'>here</a>"
+    res.send(print);
     console.log("Connected");
+});
+
+app.get('/image', function(req, res) {
+    res.send("<h1>넘모넘모 재밋는거시다!</h1><img src='/dici.jpg'>");
 });
 
 app.listen(3000, function() {
